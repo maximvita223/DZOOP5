a6dc4a707a9df046f5f34cad19c5ff1e80e8f552
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2023-01-18 20:01:05 +0300

    fixed

--- model/AttendanceService.java
+++ model/AttendanceService.java
@@ -3,10 +3,10 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AtterdanceService {
+public class AttendanceService {
     private List<Student> studentsTimesOff = new ArrayList<>();
 
-    public AtterdanceService(List<Student> studentsTimesOff) {
+    public AttendanceService(List<Student> studentsTimesOff) {
         this.studentsTimesOff = studentsTimesOff;
     }
     public List<Student> getList(){
